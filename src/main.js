e013ec5dac64cf20d823d6beaa724ee22202652e
zwh <z_wh@sina.cn>
2019-07-15 16:54:08 +0800

    完成搜过栏和焦点图

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue';
+import {
+  Button, Row, Col, Swipe, SwipeItem, Lazyload,
+} from 'vant';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+
+Vue.use(Button)
+  .use(Row)
+  .use(Col)
+  .use(Swipe)
+  .use(SwipeItem)
+  .use(Lazyload);
 
 Vue.config.productionTip = false;
 
